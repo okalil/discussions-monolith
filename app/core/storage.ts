e2d36519999fd99bbbc5aa242e0f24b1ef0415be
Kalil <kalilmagal@gmail.com>
2025-01-18 18:54:20 -0300

    wip

--- app/core/storage.ts
+++ app/core/storage.ts
@@ -1,3 +1,5 @@
+import type { FileStorage } from "@mjackson/file-storage";
+
 import { LocalFileStorage } from "@mjackson/file-storage/local";
 
-export const storage = new LocalFileStorage("./uploads");
+export const storage: FileStorage = new LocalFileStorage("./uploads");
