59a63bb260370aad418b33e6603bd15f3ea42a33
Kalil <kalilmagal@gmail.com>
2025-05-03 15:54:49 -0300

    refactor inline svg elements

--- app/web/ui/shared/button.tsx
+++ app/web/ui/shared/button.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import { cn } from "~/web/ui/shared/utils/cn";
+import { Icon } from "./icon";
+import { cn } from "./utils/cn";
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "default" | "danger";
   loading?: boolean;
@@ -26,35 +27,12 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
         ref={ref}
       >
-        {loading ? <Spinner className="animate-spin h-5 w-5" /> : children}
+        {loading ? (
+          <Icon name="spinner" size={16} className="animate-spin h-5 w-5" />
+        ) : (
+          children
+        )}
       </button>
     );
   }
 );
-
-function Spinner(props: React.ComponentProps<"svg">) {
-  return (
-    <svg
-      stroke="currentColor"
-      fill="none"
-      strokeWidth="0"
-      viewBox="0 0 24 24"
-      height="1em"
-      width="1em"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        opacity="0.2"
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-        fill="currentColor"
-      />
-      <path
-        d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-}
