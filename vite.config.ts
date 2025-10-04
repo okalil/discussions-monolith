48df932ce871bc3acbf6dd55f0fc094099793182
Kalil <kalilmagal@gmail.com>
2025-10-04 16:17:19 -0300

    added devtools json plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     reactRouter(),
     tsconfigPaths(),
+    devtoolsJson(),
   ],
   build: {
     target: "esnext",
